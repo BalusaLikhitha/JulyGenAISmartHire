e4de713c9e349364207c8d6ee68c56d63b5a04fe
Likhitha Balusa <likhithabalusa@gmail.com>
2026-09-08 18:54:42 +0530

    Remove unsupported gdown fuzzy option

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -29,7 +29,6 @@ def ensure_vectorstore():
                 url=VECTORSTORE_URL,
                 output=str(VECTORSTORE_ZIP),
                 quiet=False,
-                fuzzy=False
             )
 
             if downloaded_file is None:
